5ba6f6a666f7c323bd2b1d15e701b6a38e6f841d
Jesus Lozada <jesuselozada@gmail.com>
2025-10-27 17:02:35 -0400

    added logs

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,6 +3,19 @@ workers = 2 * multiprocessing.cpu_count() + 1
 print(workers)
 worker_class = 'eventlet'  # Use gevent async workers
 worker_connections = 2000  # Maximum concurrent connections per worker
+timeout = 30
+graceful_timeout = 30
+keepalive = 2
+
+max_requests = 1000
+max_requests_jitter = 50
+
+...
+# Logging Settings
+accesslog = "/logs_python/app-python/gunicorn_access.log"  # Log HTTP requests to a file
+errorlog = "/logs_python/app-python/gunicorn_error.log"  # Log errors to a file
+loglevel = "info"  # Set log verbosity (debug, info, warning, error, critical)
+
 
 # workers = 3
 bind = "0.0.0.0:4000"
